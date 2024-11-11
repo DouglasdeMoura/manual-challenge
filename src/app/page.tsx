086b05ef86870ecc0c44e458564c0286186bc730
Douglas Moura <douglas.ademoura@gmail.com>
2024-11-11 08:09:52 -0300

    feat: create Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import { Block } from "@/components/block";
 import { Hero } from "@/components/block/block.stories";
 import { Footer } from "@/components/footer";
 import { Default as FooterStory } from "@/components/footer/footer.stories";
+import { Header } from "@/components/header";
 import { List } from "@/components/list";
 import { Default } from "@/components/list/list.stories";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <Block {...Hero.args} />
       <List {...Default.args} />
       <Footer {...FooterStory.args} />
